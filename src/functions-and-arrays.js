d31db92140074a3ded73f69f6899a1bdabdecca2
Dicky Com <dickycom1@gmail.com>
2021-03-25 12:26:42 -0400

    iteration 8 done

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -46,8 +46,7 @@ function sumNumbers(array) {
 // Level 1: Array of numbers
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
 
-const averageNumbers = (array) =>
-	array.length !== 0 ? sumNumbers(array) / array.length : null;
+const averageNumbers = (array) => (array.length !== 0 ? sumNumbers(array) / array.length : null);
 
 // function averageNumbers(a){    //same same but different, but still the same
 //   if (a.length === 0) return null;
@@ -105,7 +104,7 @@ const wordsUnique = [
 // 	return newArr
 // }
 
-const uniquifyArray = arr => [... new Set(arr)];
+const uniquifyArray = (arr) => [...new Set(arr)];
 
 // Iteration #6: Find elements
 const wordsFind = [
@@ -121,7 +120,7 @@ const wordsFind = [
 
 //const doesWordExist = (arr, word) => arr.includes(word);
 
-function doesWordExist(arr, word){
+function doesWordExist(arr, word) {
 	return arr.includes(word);
 }
 // Iteration #7: Count repetition
@@ -138,333 +137,96 @@ const wordsCount = [
 	'disobedience',
 	'matter',
 ];
-function howManyTimes(arr, word){
+function howManyTimes(arr, word) {
 	let count = 0;
-	for ( parolla of arr ){
-		console.log(parolla, word)
-		if( parolla== word){
-			count ++
+	for (parolla of arr) {
+		console.log(parolla, word);
+		if (parolla == word) {
+			count++;
 		}
-	} 
-	return count 
-
+	}
+	return count;
 }
 
 // Iteration #8: Bonus
 
 const matrix = [
 	[8, 2, 22, 97, 38, 15, 0, 40, 0, 75, 4, 5, 7, 78, 52, 12, 50, 77, 91, 8],
-	[
-		49,
-		49,
-		99,
-		40,
-		17,
-		81,
-		18,
-		57,
-		60,
-		87,
-		17,
-		40,
-		98,
-		43,
-		69,
-		48,
-		4,
-		56,
-		62,
-		0,
-	],
-	[
-		81,
-		49,
-		31,
-		73,
-		55,
-		79,
-		14,
-		29,
-		93,
-		71,
-		40,
-		67,
-		53,
-		88,
-		30,
-		3,
-		49,
-		13,
-		36,
-		65,
-	],
+	[49, 49, 99, 40, 17, 81, 18, 57, 60, 87, 17, 40, 98, 43, 69, 48, 4, 56, 62, 0],
+	[81, 49, 31, 73, 55, 79, 14, 29, 93, 71, 40, 67, 53, 88, 30, 3, 49, 13, 36, 65],
 	[52, 70, 95, 23, 4, 60, 11, 42, 69, 24, 68, 56, 1, 32, 56, 71, 37, 2, 36, 91],
-	[
-		22,
-		31,
-		16,
-		71,
-		51,
-		67,
-		63,
-		89,
-		41,
-		92,
-		36,
-		54,
-		22,
-		40,
-		40,
-		28,
-		66,
-		33,
-		13,
-		80,
-	],
-	[
-		24,
-		47,
-		32,
-		60,
-		99,
-		3,
-		45,
-		2,
-		44,
-		75,
-		33,
-		53,
-		78,
-		36,
-		84,
-		20,
-		35,
-		17,
-		12,
-		50,
-	],
-	[
-		32,
-		98,
-		81,
-		28,
-		64,
-		23,
-		67,
-		10,
-		26,
-		38,
-		40,
-		67,
-		59,
-		54,
-		70,
-		66,
-		18,
-		38,
-		64,
-		70,
-	],
-	[
-		67,
-		26,
-		20,
-		68,
-		2,
-		62,
-		12,
-		20,
-		95,
-		63,
-		94,
-		39,
-		63,
-		8,
-		40,
-		91,
-		66,
-		49,
-		94,
-		21,
-	],
-	[
-		24,
-		55,
-		58,
-		5,
-		66,
-		73,
-		99,
-		26,
-		97,
-		17,
-		78,
-		78,
-		96,
-		83,
-		14,
-		88,
-		34,
-		89,
-		63,
-		72,
-	],
+	[22, 31, 16, 71, 51, 67, 63, 89, 41, 92, 36, 54, 22, 40, 40, 28, 66, 33, 13, 80],
+	[24, 47, 32, 60, 99, 3, 45, 2, 44, 75, 33, 53, 78, 36, 84, 20, 35, 17, 12, 50],
+	[32, 98, 81, 28, 64, 23, 67, 10, 26, 38, 40, 67, 59, 54, 70, 66, 18, 38, 64, 70],
+	[67, 26, 20, 68, 2, 62, 12, 20, 95, 63, 94, 39, 63, 8, 40, 91, 66, 49, 94, 21],
+	[24, 55, 58, 5, 66, 73, 99, 26, 97, 17, 78, 78, 96, 83, 14, 88, 34, 89, 63, 72],
 	[21, 36, 23, 9, 75, 0, 76, 44, 20, 45, 35, 14, 0, 61, 33, 97, 34, 31, 33, 95],
 	[78, 17, 53, 28, 22, 75, 31, 67, 15, 94, 3, 80, 4, 62, 16, 14, 9, 53, 56, 92],
-	[
-		16,
-		39,
-		5,
-		42,
-		96,
-		35,
-		31,
-		47,
-		55,
-		58,
-		88,
-		24,
-		0,
-		17,
-		54,
-		24,
-		36,
-		29,
-		85,
-		57,
-	],
+	[16, 39, 5, 42, 96, 35, 31, 47, 55, 58, 88, 24, 0, 17, 54, 24, 36, 29, 85, 57],
 	[86, 56, 0, 48, 35, 71, 89, 7, 5, 44, 44, 37, 44, 60, 21, 58, 51, 54, 17, 58],
-	[
-		19,
-		80,
-		81,
-		68,
-		5,
-		94,
-		47,
-		69,
-		28,
-		73,
-		92,
-		13,
-		86,
-		52,
-		17,
-		77,
-		4,
-		89,
-		55,
-		40,
-	],
+	[19, 80, 81, 68, 5, 94, 47, 69, 28, 73, 92, 13, 86, 52, 17, 77, 4, 89, 55, 40],
 	[4, 52, 8, 83, 97, 35, 99, 16, 7, 97, 57, 32, 16, 26, 26, 79, 33, 27, 98, 66],
-	[
-		88,
-		36,
-		68,
-		87,
-		57,
-		62,
-		20,
-		72,
-		3,
-		46,
-		33,
-		67,
-		46,
-		55,
-		12,
-		32,
-		63,
-		93,
-		53,
-		69,
-	],
-	[
-		4,
-		42,
-		16,
-		73,
-		38,
-		25,
-		39,
-		11,
-		24,
-		94,
-		72,
-		18,
-		8,
-		46,
-		29,
-		32,
-		40,
-		62,
-		76,
-		36,
-	],
-	[
-		20,
-		69,
-		36,
-		41,
-		72,
-		30,
-		23,
-		88,
-		34,
-		62,
-		99,
-		69,
-		82,
-		67,
-		59,
-		85,
-		74,
-		4,
-		36,
-		16,
-	],
-	[
-		20,
-		73,
-		35,
-		29,
-		78,
-		31,
-		90,
-		1,
-		74,
-		31,
-		49,
-		71,
-		48,
-		86,
-		81,
-		16,
-		23,
-		57,
-		5,
-		54,
-	],
-	[
-		1,
-		70,
-		54,
-		71,
-		83,
-		51,
-		54,
-		69,
-		16,
-		92,
-		33,
-		48,
-		61,
-		43,
-		52,
-		1,
-		89,
-		19,
-		67,
-		48,
-	],
+	[88, 36, 68, 87, 57, 62, 20, 72, 3, 46, 33, 67, 46, 55, 12, 32, 63, 93, 53, 69],
+	[4, 42, 16, 73, 38, 25, 39, 11, 24, 94, 72, 18, 8, 46, 29, 32, 40, 62, 76, 36],
+	[20, 69, 36, 41, 72, 30, 23, 88, 34, 62, 99, 69, 82, 67, 59, 85, 74, 4, 36, 16],
+	[20, 73, 35, 29, 78, 31, 90, 1, 74, 31, 49, 71, 48, 86, 81, 16, 23, 57, 5, 54],
+	[1, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 1, 89, 19, 67, 48],
 ];
+
+// function greatestProduct(matrix) {
+// 	let allArr = [];
+// 	let productArr = [];
+// 	let products = [];
+// 	let largestProduct = [];
+// 	for (let x = 0; x < matrix.length; x++) {
+// 		for (let y = 0; y <= matrix.length - 4; y++) {
+// 			let arr = [];
+// 			let arr2 = [];
+// 			for (let z = y; z < y + 4; z++) {
+// 				arr.push(matrix[x][z]);
+// 				arr2.push(matrix[z][x]);
+// 			}
+// 			allArr.push(arr);
+// 			allArr.push(arr2);
+// 		}
+// 	}
+
+// 	function multiply(a, b) {
+// 		return a * b;
+// 	}
+
+// 	allArr.forEach((arr) => {
+// 		let product = arr.reduce(multiply);
+// 		products.push(product);
+// 	});
+
+// 	products.forEach((el) => {
+// 		el > largestProduct ? (largestProduct = el) : null;
+// 	});
+
+// 	console.log(largestProduct);
+// 	return largestProduct;
+// }
+
+function greatestProduct(matrix) {
+	let sums = [];
+
+	for (row of matrix) {
+		let i = 0;
+		for (num of row) {
+			i++;
+			let resultHorizontal = num * row[i + 1] * row[i + 2] * row[i + 3];
+			let resultVertical = num * matrix[i + 1] * matrix[i + 2] * matrix[i + 3];
+			if (!isNaN(resultHorizontal)) {
+				sums.push(resultHorizontal);
+			}
+			if (!isNaN(resultVertical)) {
+				sums.push(resultVertical);
+			}
+		}
+	}
+
+	return Math.max(...sums);
+}
+
+console.log(greatestProduct(matrix));
